56893ad3c6bfe5ce46db5a93987d08a6b39e6232
jaey-oung <jaeywodud7@gmail.com>
2025-05-27 12:01:32 +0900

    refactor: ApiResponse 응답 코드 타입을 HttpStatus로 변경하고 관련 클래스 일괄 수정

--- src/main/java/kodanect/domain/remembrance/controller/BoardTestController.java
+++ src/main/java/kodanect/domain/remembrance/controller/BoardTestController.java
@@ -4,12 +4,12 @@ import kodanect.common.response.ApiResponse;
 import kodanect.domain.remembrance.entity.BoardTest;
 import kodanect.domain.remembrance.service.BoardTestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Locale;
 
 /**
  * BoardTest 컨트롤러
@@ -26,12 +26,9 @@ import java.util.Locale;
 @RequiredArgsConstructor
 @RequestMapping("/boards")
 public class BoardTestController {
-    private static final int HTTP_OK = 200;
-    private static final int HTTP_INTERNAL_SERVER_ERROR = 500;
 
-    private final MessageSource messageSource;
+    private final MessageSourceAccessor messageSourceAccessor;
     private final BoardTestService boardTestService;
-
 
     /**
      * 게시글 목록 조회 성공 시나리오
@@ -40,9 +37,9 @@ public class BoardTestController {
      */
     @GetMapping("/success")
     public ResponseEntity<ApiResponse<List<BoardTest>>> getSuccess() {
-        String successMessage = messageSource.getMessage("board.list.get.success", null, Locale.getDefault());
+        String successMessage = messageSourceAccessor.getMessage("board.list.get.success", new Object[] {});
         List<BoardTest> boards = boardTestService.success();
-        return ResponseEntity.ok(ApiResponse.success(HTTP_OK, successMessage, boards));
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK, successMessage, boards));
     }
 
     /**
@@ -53,7 +50,7 @@ public class BoardTestController {
     @GetMapping("/error/500")
     public ResponseEntity<ApiResponse<Void>> simulateInternalError() {
         boardTestService.simulateInternalError();
-        return ResponseEntity.ok(ApiResponse.fail(HTTP_INTERNAL_SERVER_ERROR, "이 코드는 도달하지 않습니다."));
+        return ResponseEntity.ok(ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR, "이 코드는 도달하지 않습니다."));
     }
 
 }
