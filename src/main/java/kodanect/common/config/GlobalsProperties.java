3aeb2c5ccc5bc3c958ce93613ee40125b61fc7e4
YOOCHANGHO <bokco99@gmail.com>
2025-06-05 09:10:30 +0900

    chore : GlobalProperties 수정_25_0605_KodanectBootApplication

--- src/main/java/kodanect/common/config/GlobalsProperties.java
+++ src/main/java/kodanect/common/config/GlobalsProperties.java
@@ -3,7 +3,6 @@ package kodanect.common.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 /**
  * 전역 설정 프로퍼티 클래스
