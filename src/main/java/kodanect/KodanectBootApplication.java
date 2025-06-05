9633c0f32f893138e607a2b9fceeeb057d4c6ede
YOOCHANGHO <bokco99@gmail.com>
2025-06-05 11:04:37 +0900

    chore : 사진 삭제

--- src/main/java/kodanect/KodanectBootApplication.java
+++ src/main/java/kodanect/KodanectBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@ConfigurationPropertiesScan
+//@ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableConfigurationProperties(GlobalsProperties.class)
 public class KodanectBootApplication {
